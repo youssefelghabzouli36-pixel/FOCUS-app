3ab71209bb150ecb796ebe3914c451e6dabb1bcf
youssefelghabzouli36-pixel <youssefelghabzouli36@gmail.com>
2026-09-12 12:31:47 +0200

    Update App.js

--- App.js
+++ App.js
@@ -6,6 +6,20 @@
 // @expo/vector-icons
 
 import React, { useEffect, useMemo, useState } from 'react';
+export default function App() {
+    useEffect(() => {
+    if (typeof document !== 'undefined') {
+      let appleIcon = document.querySelector('link[rel="apple-touch-icon"]');
+
+      if (!appleIcon) {
+        appleIcon = document.createElement('link');
+        appleIcon.rel = 'apple-touch-icon';
+        document.head.appendChild(appleIcon);
+      }
+
+      appleIcon.href = '/apple-touch-icon.png';
+    }
+  }, []);
 import {
   SafeAreaView,
   View,
